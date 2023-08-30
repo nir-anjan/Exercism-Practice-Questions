084a88debc35cc45114a06cdf4f81dd14a70a1a8
Niranajn P Shaji <108530839+nir-anjan@users.noreply.github.com>
2023-08-30 23:59:34 +0530

    Create raindrop.cpp

---
+++ 6_Raindrop/raindrop.cpp
@@ -0,0 +1,19 @@
+#include "raindrops.h"
+
+namespace raindrops {
+
+    std::string convert(int no){
+        std::string result = "";
+        if(no%3==0)
+            result += "Pling";
+         if(no%5==0)
+            result += "Plang";
+         if(no%7==0)
+            result += "Plong";
+        if(!(no%3==0) && !(no%5==0) && !(no%7==0))
+            return std::to_string(no);
+        else
+            return result;
+        }
+    }
+
